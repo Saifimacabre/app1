264b37f7e6158f0a70b02fcb2459918fd3beb7a8
Saief Mohammad Amanullah <123192464+Saifimacabre@users.noreply.github.com>
2025-06-25 03:17:44 +0600

    streamlit_crop_fertilizer_app.py

--- streamlit_crop_fertilizer_app.py
+++ streamlit_crop_fertilizer_app.py
@@ -6,8 +6,8 @@ from sklearn.preprocessing import LabelEncoder
 # Load datasets
 @st.cache_data
 def load_data():
-    fert_df = pd.read_csv("data/Fertilizer Prediction.csv")
-    crop_df = pd.read_csv("data/Crop_recommendation.csv")
+    fert_df = pd.read_csv("Fertilizer Prediction.csv")
+    crop_df = pd.read_csv("Crop_recommendation.csv")
     return fert_df, crop_df
 
 fertilizer_df, crop_recommendation_df = load_data()
